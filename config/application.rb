4dbe6cbce4e1593ac40bd4ce8b42e84126151148
Celena T <chibiynm@gmail.com>
2016-02-25 15:06:25 -0500

    Rename application and database

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "action_controller/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsApiTemplate
+module BubbleTeaApi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
