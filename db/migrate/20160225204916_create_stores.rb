8cfa6e43d19ac36999878dd0271552ab162f3ba4
Celena T <chibiynm@gmail.com>
2016-02-25 15:50:38 -0500

    Create stores tale, store migration, and store model

---
+++ db/migrate/20160225204916_create_stores.rb
@@ -0,0 +1,11 @@
+class CreateStores < ActiveRecord::Migration
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :locations
+      t.text :tips
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724141255) do
+ActiveRecord::Schema.define(version: 20160225204916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "name"
+    t.string   "locations"
+    t.text     "tips"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
