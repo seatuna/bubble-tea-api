da92e1b847d900f77751687135fd4ecbf4921d0a
Celena T <chibiynm@gmail.com>
2016-02-26 11:50:32 -0500

    Add drinks routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  resources :drinks
 end
