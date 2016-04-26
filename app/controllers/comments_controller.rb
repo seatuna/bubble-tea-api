e556ad38ff01afa5ad60dd83529fd06bcda069bd
Celena T <chibiynm@gmail.com>
2016-04-26 19:49:43 -0400

    Generate comments controller and serializer

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,62 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:update, :destroy]
+
+  def index
+    @comments = Comment.all
+    render json: @comments
+  end
+
+  # GET /comments/1
+  def show
+    @comment = Comment.find(params[:id]);
+    render json: @comment
+  end
+
+  # POST /comments
+  def create
+
+    if current_user.admin?
+      @comment = current_user.drinks.find(params[:store_id]).comments.build(comment_params)
+      @comment.user_id = current_user.id
+
+      if @comment.save
+        render json: @comment, status: :created, location: @comment
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
+
+    else
+      head :bad_request
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  def update
+    @comment.update(comment_params)
+    if @comment.valid?
+      head :no_content
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /comments/1
+  def destroy
+    @comment.destroy
+    if @comment.destroyed?
+      head :no_content
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def set_comment
+      @comment = current_user.comments.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comments).permit(:text)
+    end
+end
