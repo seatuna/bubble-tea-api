8999dd1da8d14fb7b2cc7e2a2705c8a7aeeb75f2
Celena T <chibiynm@gmail.com>
2016-04-26 22:05:55 -0400

    Add working patch and delete curl scripts

---
+++ scripts/delete-comment.sh
@@ -0,0 +1,2 @@
+curl --include --request DELETE http://localhost:3000/comments/13 \
+  --header "Authorization: Token token=d9355d306c8ca9848454e390772c7a52" \
--- scripts/patch-comment.sh
+++ scripts/patch-comment.sh
@@ -1,8 +1,8 @@
-curl --include --request POST http://localhost:3000/drinks/2/comments \
+curl --include --request PATCH http://localhost:3000/comments/13 \
   --header "Authorization: Token token=d9355d306c8ca9848454e390772c7a52" \
   --header "Content-Type: application/json" \
   --data '{
     "comments": {
-      "comment": "Nope it can be as long as I want"
+      "comment": "Test tea"
     }
   }'
